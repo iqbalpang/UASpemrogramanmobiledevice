886713ee72fb6058513ed209d77c6c655a918483
iqbalpang <muhamad.iqbal5262@gmail.com>
2023-06-13 02:09:35 +0700

    sementara

--- app/src/main/java/com/example/uaspemrogramanmobiledevice/MainActivity.java
+++ app/src/main/java/com/example/uaspemrogramanmobiledevice/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.uaspemrogramanmobiledevice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+   Button btnok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-}
+        btnok = (Button) findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity1.class);
+                startActivity(intent);
+            }
+        });
+    }}
---
+++ app/src/main/java/com/example/uaspemrogramanmobiledevice/MainActivity1.java
@@ -0,0 +1,38 @@
+package com.example.uaspemrogramanmobiledevice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity1 extends AppCompatActivity {
+    private Spinner Spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+       Spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.pilih, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner.setAdapter(adapter);
+        Spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), adapter.getItem(i), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+    }
+}
